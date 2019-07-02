907ae1822ac0bcbe520a7ae2fe314190cd678345
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2019-07-01 20:49:01 -0500

    feat: Import BooksAPI, set state by calling .getAll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchPage from "./SearchPage";
 import MainPage from "./MainPage";
 
 class BooksApp extends React.Component {
+    state ={
+        books: []
+    };
+
+    componentDidMount(){
+        BooksAPI.getAll()
+            .then((books)=>{
+                this.setState({books: books})
+            })
+    }
   render() {
     return (
          <MainPage />
